61e8f0a2df808aa470d62e4ec0690a1fac02e229
alexidr9116 <alexdr9116@gmail.com>
2022-07-15 05:12:24 -0700

    bone

---
+++ src/_mock/Membership.js
@@ -0,0 +1,56 @@
+export  const MembershipData = [
+    {
+        name:"Platinum",
+        description:"100K & Above",
+        dashColor:"#f49d37",
+        benifies:[
+            {title:"Referal Program", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Transaction Fee Discount", subTitle:"1.5%", icon:"cil:hand-point-right"},
+            {title:"Member Events & meetups", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Exclusive Discord Channel", subTitle:"", icon:"cil:hand-point-right"},
+            {title:`Exclusive Offers `, subTitle:"*Subject to Avaliability", icon:"cil:hand-point-right"},
+            {title:"VIP First Lock ", subTitle:"3days ahead", icon:"cil:hand-point-right"}
+        ]
+    },
+    {
+        name:"Gold",
+        description:"50K-100K",
+        dashColor:"#5B2F02 ",
+        benifies:[
+            {title:"Referal Program", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Transaction Fee Discount", subTitle:"1.5%", icon:"cil:hand-point-right"},
+            {title:"Member Events & meetups", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Exclusive Discord Channel", subTitle:"", icon:"cil:hand-point-right"},
+            {title:`Exclusive Offers `, subTitle:"*Subject to Avaliability", icon:"cil:hand-point-right"},
+            {title:"VIP First Lock ", subTitle:"3days ahead", icon:"cil:hand-point-right"}
+        ]
+    },
+    {
+        name:"Silver",
+        description:"500-49K",
+        dashColor:"#394047 ",
+        benifies:[
+            {title:"Referal Program", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Transaction Fee Discount", subTitle:"1.5%", icon:"cil:hand-point-right"},
+            {title:"Member Events & meetups", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Exclusive Discord Channel", subTitle:"", icon:"cil:hand-point-right"},
+            {title:`Exclusive Offers `, subTitle:"*Subject to Avaliability", icon:"cil:hand-point-right"},
+            {title:"VIP First Lock ", subTitle:"3days ahead", icon:"cil:hand-point-right"}
+        ]
+    },
+    {
+        name:"Starter",
+        description:"0-500",
+        dashColor:"#D3D3D3 ",
+        benifies:[
+            {title:"Referal Program", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Transaction Fee Discount", subTitle:"1.5%", icon:"cil:hand-point-right"},
+            {title:"Member Events & meetups", subTitle:"", icon:"cil:hand-point-right"},
+            {title:"Exclusive Discord Channel", subTitle:"", icon:"cil:hand-point-right"},
+            {title:`Exclusive Offers `, subTitle:"*Subject to Avaliability", icon:"cil:hand-point-right"},
+            {title:"VIP First Lock ", subTitle:"3days ahead", icon:"cil:hand-point-right"}
+        ]
+    },
+    
+]
+
