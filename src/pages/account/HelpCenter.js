57bcbfc5940a67cde2b8444f16cdaa06b74d3b76
alexidr9116 <alexdr9116@gmail.com>
2022-07-18 06:54:13 -0700

    help center

--- src/pages/account/HelpCenter.js
+++ src/pages/account/HelpCenter.js
@@ -4,11 +4,12 @@ import { useEffect } from 'react';
 import Page from "../../components/Page";
 import StayInLoop from "../../sections/StayInLoop";
 
+
 export default function HelpCenter() {
     return (
         <Page title = "Help Center">
             <Box sx={{ display: 'flex', gap: { xs: 10, sm: 12, md: 14 }, flexDirection: 'column' }} >
-                >
+          
                 {/* Stay in loop start */}
                 <StayInLoop />
             </Box>
