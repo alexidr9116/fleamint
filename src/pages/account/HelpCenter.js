46e4c042744ebdae23f7aa09cf3d07223c2dd9cb
alexidr9116 <alexdr9116@gmail.com>
2022-07-18 12:37:28 -0700

    help center

--- src/pages/account/HelpCenter.js
+++ src/pages/account/HelpCenter.js
@@ -1,18 +1,34 @@
-import { Box, Container, Grid, } from '@mui/material';
+import { Box, Container, Grid, Stack, Typography, useTheme } from '@mui/material';
 import { useEffect } from 'react';
 
 import Page from "../../components/Page";
 import StayInLoop from "../../sections/StayInLoop";
+import { GradientStyle } from '../Home';
 
 
 export default function HelpCenter() {
+    const theme = useTheme();
+
     return (
-        <Page title = "Help Center">
-            <Box sx={{ display: 'flex', gap: { xs: 10, sm: 12, md: 14 }, flexDirection: 'column' }} >
-                
-                {/* Stay in loop start */}
-                <StayInLoop />
-            </Box>
+        <Page title="Help Center">
+            <GradientStyle sx={{ paddingTop: {xs:15, sm:20}, paddingBottom: 4, display: 'flex', gap: 8, flexDirection: 'column' }} >
+                <Container >
+                    <Box xs={{ display: 'relative' }}>
+                        <Grid container>
+                            <Grid item xs={12} sm={6}>
+                                <Box>
+                                    <Typography variant={'h3'} component = {'p'}>
+                                        Discover, Collect, And Sell <font color = {theme.palette.primary.dark}>NFTs</font>
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Container>
+            </GradientStyle>
+            {/* Stay in loop start */}
+            <StayInLoop />
+
         </Page>
     )
 }
